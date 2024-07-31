c409166a221e62a4c05bac40ef55653e5bb34bb9
nossila2 <101668417+nossila2@users.noreply.github.com>
2024-07-30 23:04:48 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -21,8 +21,8 @@ provider "azurerm" {
   
   features {
   }
-  subscription_id = "3df3bf53-d19c-4447-a202-9d092533e1b2"
-  tenant_id = "14cbd5a7-ec94-46ba-b314-cc0fc972a161"
-  client_id = "4e398c69-6030-4487-a301-99d27b0a49b9"
-  client_secret = "yVh8Q~c4fERtMwblG6kT3N0Z2JRfQlGGYSZ2.c.K"
+  subscription_id = "xxxxxxxxxxxxxxxxxxxxxx"
+  tenant_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_id = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
