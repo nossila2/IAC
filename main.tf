713b716b0bc50e10eb5595048fd4530354334cac
nossila2 <101668417+nossila2@users.noreply.github.com>
2024-07-30 21:40:33 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "vnet" {
 # Cria subnets
 resource "azurerm_subnet" "subnet" {
   name                 = "aula-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.student-rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
